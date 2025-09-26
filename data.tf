8a4d0eb7239295c8a9c3d26d0a8bdf86ac766dbe
Sven Skender <sskender@pm.me>
2025-09-26 17:07:39 +0200

    Add IAM and security group resources

--- data.tf
+++ data.tf
@@ -1,3 +1,22 @@
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+data "aws_iam_policy" "ssm_core" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
---
+++ server.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "server" {
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "server_ssm_attachment" {
+  role       = aws_iam_role.server.name
+  policy_arn = data.aws_iam_policy.ssm_core.arn
+}
+
+resource "aws_iam_instance_profile" "server" {
+  role = aws_iam_role.server.name
+}
+
+resource "aws_security_group" "server" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "server_allow_all" {
+  security_group_id = aws_security_group.server.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "server_allow_dns_tcp" {
+  security_group_id = aws_security_group.server.id
+  from_port         = 53
+  to_port           = 53
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "server_allow_dns_udp" {
+  security_group_id = aws_security_group.server.id
+  from_port         = 53
+  to_port           = 53
+  ip_protocol       = "udp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
